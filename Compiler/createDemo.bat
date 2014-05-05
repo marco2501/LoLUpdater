eca7e2b3998cc2f422f60fea64cb32ac0dbffcd9
Ilja Korsun <ilja.korsun@gmail.com>
2014-05-05 22:35:12 +0200

    asdasd

--- Compiler/createDemo.bat
+++ Compiler/createDemo.bat
@@ -1,1 +1,1 @@
-call "callPS2EXE.bat" "lolupdater.ps1" "LoLUpdater.exe" -sta -x86 -noconsole -runtime20
+call "callPS2EXE.bat" "lolupdater.ps1" "LoLUpdater.exe" -sta -x86 -noconsole -runtime20 -verbose
