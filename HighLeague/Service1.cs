cc1a7d4dc13b6ff43d1b0d3216b9e3a1c957fcea
Ilja Korsun <ilja.korsun@gmail.com>
2014-05-05 14:58:52 +0200

    Set service priority to below normal

--- HighLeague/Service1.cs
+++ HighLeague/Service1.cs
@@ -10,45 +10,34 @@ using System.Threading.Tasks;
 
 namespace HighLeague
 {
-    public partial class HighLeague : ServiceBase
-    {
-        private System.Timers.Timer timer;
-        public HighLeague()
-        {
-            this.ServiceName = "HighLeague";
-            InitializeComponent();
-        }
-        protected override void OnStart(string[] args)
-        {
-
-            this.timer = new System.Timers.Timer(30000D);  // 30000 milliseconds = 30 seconds
-            this.timer.AutoReset = true;
-            this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
-            this.timer.Start();
-
-
-
-        }
-
-        protected override void OnStop()
-        {
-            this.timer.Stop();
-            this.timer = null;
-        }
-        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            Process[] processes = Process.GetProcessesByName("League of Legends");
-
-
-            foreach (Process process in processes)
-            {
-
-                process.PriorityClass = ProcessPriorityClass.High;
-
-
-
-            }
-
-        }
-    }
+public partial class HighLeague : ServiceBase
+{
+private System.Timers.Timer timer;
+public HighLeague()
+{
+ this.ServiceName = "HighLeague";
+InitializeComponent();
+}
+ protected override void OnStart(string[] args)
+{
+System.Diagnostics.Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.BelowNormal;
+this.timer = new System.Timers.Timer(30000D);
+this.timer.AutoReset = true;
+this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
+this.timer.Start();
+}
+protected override void OnStop()
+{
+this.timer.Stop();
+this.timer = null;
+}
+private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+{
+Process[] processes = Process.GetProcessesByName("League of Legends");
+foreach (Process process in processes)
+{
+process.PriorityClass = ProcessPriorityClass.High;
+}
+}
+}
 }
