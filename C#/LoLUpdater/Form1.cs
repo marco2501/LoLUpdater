dc8b3278f4f3bb91bc327842668ba6a40e26c95a
Ilja Korsun <ilja.korsun@gmail.com>
2014-05-06 23:18:21 +0200

    sss

--- C#/LoLUpdater/Form1.cs
+++ C#/LoLUpdater/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 
 namespace LoLUpdater
@@ -72,7 +73,8 @@ namespace LoLUpdater
         {
             string Rads = @"Dir\Rads";
             System.IO.Directory.Exists(Rads);
-            {}
+            {
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -88,6 +90,20 @@ namespace LoLUpdater
         private void Form1_Load(object sender, EventArgs e)
         {
             var Dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            DirectoryInfo airinfo = new DirectoryInfo(@"Dir\RADS\projects\lol_air_client\releases");
+            DirectoryInfo air = airinfo.GetDirectories().OrderByDescending(f => f.CreationTime).FirstOrDefault();
+
+            DirectoryInfo slninfo = new DirectoryInfo(@"Dir\RADS\solutions\lol_game_client_sln\releases");
+            DirectoryInfo sln = slninfo.GetDirectories().OrderByDescending(f => f.CreationTime).FirstOrDefault();
+
+            DirectoryInfo gameinfo = new DirectoryInfo(@"Dir\RADS\projects\lol_game_client\releases");
+            DirectoryInfo game = gameinfo.GetDirectories().OrderByDescending(f => f.CreationTime).FirstOrDefault();
+
+            DirectoryInfo launchinfo = new DirectoryInfo(@"Dir\RADS\projects\lol_launcher\releases");
+            DirectoryInfo launch = launchinfo.GetDirectories().OrderByDescending(f => f.CreationTime).FirstOrDefault();
+
+
         }
     }
 }
