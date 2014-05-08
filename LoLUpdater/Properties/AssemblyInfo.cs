ce73f12eaf74621dc87c2f9e07f2d09d05a71ad4
Ilja Korsun <ilja.korsun@gmail.com>
2014-05-08 21:17:47 +0200

    Update

--- LoLUpdater/Properties/AssemblyInfo.cs
+++ LoLUpdater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LoLUpdater")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Ilja Korsun")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
